17748f1452e2d0436e0823f2821e44fc5686ce50
Harrison Itotia <harrisonitotia@gmail.com>
2023-08-20 15:50:32 +0300

    add dark-light mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,30 @@
-import Header from './components/Header'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Providers from "./Providers";
+import Header from "./components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'MovieFlix',
-  description: 'This is the official MovieFlix website',
-}
+  title: "MovieFlix",
+  description: "This is the official MovieFlix website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body>
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Header */}
-        <Header/>
+          {/* Navbar */}
 
-        {/* Navbar */}
+          {/* SearchBox */}
 
-        {/* SearchBox */}
-        
-        {children}
-        </body>
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
